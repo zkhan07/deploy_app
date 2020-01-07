09ed6a542a5fde599f03824e8ffdc608795463dd
zaid <khanzaid786786.kz@gmail.com>
2020-01-07 12:58:16 +0530

    Updates

---
+++ precache-manifest.e300f5f4a0086cc6e5a202e00990b58f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11adc0226bab083968ed9e7045830c95",
+    "url": "/deploy_app/index.html"
+  },
+  {
+    "revision": "24cfc8d5f1d793bf8e1e",
+    "url": "/deploy_app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "840e42e7ae2ecbb8a574",
+    "url": "/deploy_app/static/js/2.d277c0a8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/deploy_app/static/js/2.d277c0a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24cfc8d5f1d793bf8e1e",
+    "url": "/deploy_app/static/js/main.1455308c.chunk.js"
+  },
+  {
+    "revision": "da9ce61d61f43b3ce6bd",
+    "url": "/deploy_app/static/js/runtime-main.85f7e76e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
