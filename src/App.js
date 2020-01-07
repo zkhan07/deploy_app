59be5bd55f6e621bf2adfd9457a0c48f6450effe
zaid <khanzaid786786.kz@gmail.com>
2020-01-07 16:40:43 +0530

    adeed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "./Style.css";
+import Page1 from "./Page1"
+
+import Dashboard from "./Dashboard";
+import Card from "./Card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dashboard />
+      <Card />
+
+      <Page1 />
     </div>
   );
 }
